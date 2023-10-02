fdcced53f25656f13e58e80dff620f77ffcd301e
toir02 <wend8765@gmail.com>
2023-10-02 23:33:15 +0300

    added models for education app

--- education/models.py
+++ education/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {
+    'blank': True,
+    'null': True
+}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100, verbose_name='название')
+    image = models.ImageField(upload_to='course/', verbose_name='превью', **NULLABLE)
+    description = models.TextField(verbose_name='описание')
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=100, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    image = models.ImageField(upload_to='lesson/', verbose_name='превью', **NULLABLE)
+    link = models.TextField(verbose_name='ссылка на видео')
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
