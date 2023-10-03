5ea6412f05720d3312768b48308f3cc273b1d407
toir02 <wend8765@gmail.com>
2023-10-03 12:47:19 +0300

    added lesson create api view

--- education/views.py
+++ education/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
 from education.models import Course
-from education.serializers import CourseSerializer
+from education.serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+
+
+class LessonCreateAPIView(generics.CreateAPIView):
+    serializer_class = LessonSerializer
