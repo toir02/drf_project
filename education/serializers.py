d6ccf59fbfedff84b7345be543fc83f09fb75321
toir02 <wend8765@gmail.com>
2023-10-03 11:39:38 +0300

    added course serializer

---
+++ education/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from education.models import Course
+
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Course
+        fields = '__all__'
