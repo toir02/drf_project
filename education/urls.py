011f966a0025d0125960eee5bdccebad0c76fb89
toir02 <wend8765@gmail.com>
2023-10-03 13:34:01 +0300

    added lesson list path

--- education/urls.py
+++ education/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from education.apps import EducationConfig
-from education.views import CourseViewSet, LessonCreateAPIView
+from education.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView
 
 app_name = EducationConfig.name
 
@@ -10,5 +10,6 @@ router = DefaultRouter()
 router.register(r'course', CourseViewSet, basename='course')
 
 urlpatterns = [
-    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create')
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lesson/', LessonListAPIView.as_view(), name='lesson_list'),
 ] + router.urls
