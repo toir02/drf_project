9e1f6f438b9a47ef160a2e3150cefc49a076bcd1
toir02 <wend8765@gmail.com>
2023-10-03 11:44:12 +0300

    register router

--- education/urls.py
+++ education/urls.py
@@ -1,10 +1,12 @@
 from rest_framework.routers import DefaultRouter
 
 from education.apps import EducationConfig
+from education.views import CourseViewSet
 
 app_name = EducationConfig.name
 
 router = DefaultRouter()
+router.register(r'course', CourseViewSet, basename='course')
 
 urlpatterns = [
 
