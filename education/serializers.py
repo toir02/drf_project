8851f0d9ce5f72a71e8ece4f43ac734021074b46
toir02 <wend8765@gmail.com>
2023-10-04 21:04:58 +0300

    added get lessons count method

--- education/serializers.py
+++ education/serializers.py
@@ -4,6 +4,11 @@ from education.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_lessons_count(instance):
+        return instance.lessons.count()
 
     class Meta:
         model = Course
