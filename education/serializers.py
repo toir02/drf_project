02180475643361f3599b0cfbaf714d658d3f415b
toir02 <wend8765@gmail.com>
2023-10-05 23:46:16 +0300

    added lessons set to course serializer

--- education/serializers.py
+++ education/serializers.py
@@ -3,8 +3,16 @@ from rest_framework import serializers
 from education.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
 
     @staticmethod
     def get_lessons_count(instance):
@@ -12,11 +20,4 @@ class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = '__all__'
-
-
-class LessonSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Lesson
         fields = '__all__'
