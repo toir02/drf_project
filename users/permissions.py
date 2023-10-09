b2064c9c990c8bf883b6091482a2c54ad58eed17
toir02 <wend8765@gmail.com>
2023-10-10 01:01:00 +0300

    added permission is moderator

--- users/permissions.py
+++ users/permissions.py
@@ -4,3 +4,13 @@ from rest_framework import permissions
 class IsUnauthenticated(permissions.BasePermission):
     def has_permission(self, request, view):
         return not request.user.is_authenticated
+
+
+class IsModerator(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name='moderators').exists()
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return False
