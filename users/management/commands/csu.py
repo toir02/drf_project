062b2a320fb49f5692a126472c2f65bd3f57b249
toir02 <wend8765@gmail.com>
2023-10-09 22:59:29 +0300

    added command for create super user

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,7 +5,7 @@ from users.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user = User.objects.create(email='admin@admin.ru',
+        user = User.objects.create(email='test@admin.ru',
                                    is_staff=True,
                                    is_superuser=True,
                                    is_active=True)
