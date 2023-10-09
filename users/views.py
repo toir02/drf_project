57b1b9c640e580af95479630ac6164c0f55a1553
toir02 <wend8765@gmail.com>
2023-10-10 00:47:03 +0300

    added IsUnauthenticated custom permission to user create api view

--- users/views.py
+++ users/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from rest_framework import generics
 
 from users.models import User
+from users.permissions import IsUnauthenticated
 from users.serializers import UserSerializer
 
 
 class UserCreateAPIView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [IsUnauthenticated]
 
 
 class UserListAPIView(generics.ListAPIView):
