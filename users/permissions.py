cd23f9b3be451515d77a5374cbc8998203b5f622
toir02 <wend8765@gmail.com>
2023-10-10 01:10:00 +0300

    added custom permission is owner

--- users/permissions.py
+++ users/permissions.py
@@ -14,3 +14,8 @@ class IsModerator(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return False
+
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
