d57179dafd5833d6c2b1de83c937da65965a810c
toir02 <wend8765@gmail.com>
2023-10-09 22:34:57 +0300

    added endpoints to token

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 
@@ -12,4 +13,6 @@ urlpatterns = [
     path('view/<int:pk>/', UserRetrieveAPIView.as_view(), name='view_user'),
     path('edit/<int:pk>/', UserUpdateAPIView.as_view(), name='edit_user'),
     path('delete/<int:pk>/', UserDestroyAPIView.as_view(), name='delete_user'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
