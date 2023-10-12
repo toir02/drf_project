6f50c5e9118bf0fb288d887bd937b8eee1a1a48d
toir02 <wend8765@gmail.com>
2023-10-12 15:11:56 +0300

    refactor paths

--- education/urls.py
+++ education/urls.py
@@ -14,9 +14,9 @@ router.register(r'payment', PaymentViewSet, basename='payment')
 urlpatterns = [
     path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
     path('lesson/', LessonListAPIView.as_view(), name='lesson_list'),
-    path('lesson/view/<int:pk>', LessonRetrieveAPIView.as_view(), name='lesson_view'),
-    path('lesson/update/<int:pk>', LessonUpdateAPIView.as_view(), name='lesson_edit'),
-    path('lesson/delete/<int:pk>', LessonDestroyAPIView.as_view(), name='lesson_delete'),
+    path('lesson/view/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_view'),
+    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_edit'),
+    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
     path('subscriptions/create/', SubscriptionCreateAPIView.as_view(), name='create-subscription'),
     path('subscriptions/delete/<int:pk>/', SubscriptionDestroyAPIView.as_view(), name='delete-subscription'),
 ] + router.urls
