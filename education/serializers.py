c16faaf6e4d3c684b3556ff2bd5360d0638518db
toir02 <wend8765@gmail.com>
2023-10-12 14:16:40 +0300

    added link validator

--- education/serializers.py
+++ education/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from education.models import Course, Lesson, Payment
+from education.validators import LinkValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
@@ -8,6 +9,7 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [LinkValidator(field='link')]
 
 
 class CourseSerializer(serializers.ModelSerializer):
