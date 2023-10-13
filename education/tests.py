1ee8d092d5e46bbfe524f58f54203b3e5db291a0
toir02 <wend8765@gmail.com>
2023-10-14 01:57:45 +0300

    added test create lesson

--- education/tests.py
+++ education/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from education.models import Lesson
+
+
+class LessonTestCase(APITestCase):
+
+    def setUp(self):
+        pass
+
+    def test_create_lesson(self):
+        """test for create lesson"""
+        data = {
+            "title": "test",
+            "description": "test",
+            "link": "https://www.youtube.com/watch?v=_x8DV1WLtks&t=142s",
+        }
+
+        url = reverse("education:lesson_create")
+        response = self.client.post(url, data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        self.assertEqual(response.json(), {"id": 1,
+                                           "title": "test",
+                                           "description": "test",
+                                           "image": None,
+                                           "link": "https://www.youtube.com/watch?v=_x8DV1WLtks&t=142s",
+                                           "user": None,
+                                           "course": None})
+
+    def test_get_lessons(self):
+        """test for get lessons"""
+        url = reverse("education:lessons")
