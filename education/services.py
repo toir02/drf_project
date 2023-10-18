b172bb0af73d1fc57a6a99209b58c17fce64109f
toir02 <wend8765@gmail.com>
2023-10-18 19:14:44 +0300

    added services for stripe

---
+++ education/services.py
@@ -0,0 +1,18 @@
+import requests
+
+from config.settings import STRIPE_API_KEY
+
+
+def creates_payment_intent(amount, currency='rub'):
+    headers = {'Authorization': f'Bearer {STRIPE_API_KEY}'}
+    params = {'amount': amount, 'currency': currency}
+    response = requests.post('https://api.stripe.com/v1/payment_intents', headers=headers, params=params)
+    data = response.json()
+    return data['id']
+
+
+def retrieve_payment_intent(payment_id):
+    headers = {'Authorization': f'Bearer {STRIPE_API_KEY}'}
+    response = requests.get(f'https://api.stripe.com/v1/payment_intents/{payment_id}', headers=headers)
+
+    return response.json()
