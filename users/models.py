cf618925a31833818311b8234aad1fc15ed31558
toir02 <wend8765@gmail.com>
2023-10-19 23:52:25 +0300

    deleted email redef

--- users/models.py
+++ users/models.py
@@ -6,12 +6,8 @@ NULLABLE = {'blank': True,
 
 
 class User(AbstractUser):
-    username = None
     email = models.EmailField(unique=True, verbose_name='почта')
 
     avatar = models.ImageField(upload_to='users/', **NULLABLE)
     phone = models.CharField(max_length=35, verbose_name='номер телефона', **NULLABLE)
     city = models.CharField(max_length=40, verbose_name='город')
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import generics
 
 from users.models import User
