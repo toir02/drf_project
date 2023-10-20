9d3b19fe78db7eb731aa16c728ea02f7614b6f14
toir02 <wend8765@gmail.com>
2023-10-21 01:46:30 +0300

    added celery beat to installed apps

--- config/settings.py
+++ config/settings.py
@@ -38,10 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'drf_yasg',
 
+    'drf_yasg',
     'rest_framework',
     'django_filters',
+    'django_celery_beat'
 
     'users',
     'education',
